b8d3c3d6f002a6749ef1fded93a0a6f7655c7017
Gshihao <100885352+Gshihao@users.noreply.github.com>
2022-12-25 12:13:28 +0800

    Add files via upload

---
+++ js/dome.js
@@ -0,0 +1,23 @@
+function guanbi(){
+   document.getElementById("backstagediva").style.display="none";
+}
+function denglu(){
+  let user=document.getElementById("fromuser").value;
+  let pass=document.getElementById("frompass").value;
+  if(user.length !=0 && pass.length !=0){
+       var ajax = new XMLHttpRequest();
+       ajax.open("post","https://sl.hlunn.com/api/login");
+       ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+       ajax.send("appId=2148&user="+user+"&password="+pass+"&device=webhtml001");
+       ajax.onreadystatechange = function () {
+         if (ajax.readyState == 4 && ajax.status == 200) {
+               var  poa= ajax.responseText;
+               var obje=JSON.parse(poa);
+               if(obje.code ==200){
+                  alert(obje.result);
+                  window.open("demo.html")
+                  
+               }
+              }}}
+  
+}

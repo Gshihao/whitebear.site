040e860774857d874febf4682d5df315789e65d5
GSHAO <1462905973@qq.com>
2023-10-11 21:47:08 +0800

    修改了部分bug

---
+++ js/look/pic.js
@@ -0,0 +1,9 @@
+// console.log(452);
+const imgon = document.querySelectorAll("img")
+console.log(imgon);
+imgon.forEach((item,index)=>{
+    console.log(item);
+  item.addEventListener("click",function(e){
+      console.log(e);
+  })
+})

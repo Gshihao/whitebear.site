ee514dda3c6104932f4ed923d1a949839487beb0
GSHAO <1462905973@qq.com>
2023-10-12 21:57:57 +0800

    写完了一部分img图片点击放大，没有完成功能的实现

--- js/look/pic.js
+++ js/look/pic.js
@@ -2,8 +2,20 @@
 const imgon = document.querySelectorAll("img")
 // console.log(imgon);
 imgon.forEach((item, index) => {
-    // console.log(item);
     item.addEventListener("click", function (e) {
         console.log(e.target.src);
+        let src = e.target.src;
+
+        // 图片预览
+        // let pichtml = `<div><img src="" alt=""></div>`
+        // 创建div
+        const divpic = document.createElement("div");
+        divpic.classList.add("divpic","picnbod")
+        // 创建img
+        const divimg = document.createElement("IMG");
+        divimg.src = src
+        // 将图片插入 div
+        divpic.appendChild(divimg)
+        document.body.appendChild(divpic)
     })
 })
